8b319afd32b4c2ee8f3b2b37948c3cbcb472c691
7nimor <nima7playstation4@gmail.com>
2023-10-20 15:56:17 +0330

    create comment form

--- home/forms.py
+++ home/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from account.models import Post
+from .models import Comments
 
 
 class PostCreateUpdateForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('body',)
+
+
+class CommentCreateForm(forms.ModelForm):
+    class Meta:
+        model = Comments
+        fields = ('body',)
+        widgets = {
+            'body': forms.TextInput(attrs={'class':"form-control", 'id':"exampleFormControlTextarea1"}),
+        }
